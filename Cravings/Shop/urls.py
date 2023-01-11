cb073034335bb4947c9cddca9c05ac8a5eeaa26d
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-01-11 23:18:23 +0530

    Hero section

--- Cravings/Shop/urls.py
+++ Cravings/Shop/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path,include
 urlpatterns=[
-    path('',views.index,name='Home')
+    path('',views.index,name='Home'),
+    path('test',views.test,name='Test')
 ]
