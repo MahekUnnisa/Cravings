4a79057b914f7e62e6fabe92bb6234ebf5f930b0
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-01-13 23:48:56 +0530

    Configured views,urls and templates

--- Cravings/Shop/urls.py
+++ Cravings/Shop/urls.py
@@ -1,7 +1,10 @@
 from . import views
-from django.urls import path,include
+from django.urls import path
+
 urlpatterns=[
     path('',views.index,name='Home'),
-    path('test',views.test,name='Test'),
+    path('about',views.about,name='AboutUs'),
+    path('contact',views.contact,name='ContactUs'),
     path('recepies',views.recepies,name='Recepies'),
+    path('test',views.test,name='Test'),
 ]
--- Cravings/Shop/views.py
+++ Cravings/Shop/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def test(request):
-    return render(request, 'test.html')
+def about(request):
+    return render(request, 'about.html')
 
 def recepies(request):
     return render(request, 'recepies.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def test(request):
+    return render(request, 'test.html')
