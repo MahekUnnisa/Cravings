3e0ec28262b76d34c8201c7b21a29111ce09ba12
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-01-18 22:58:28 +0530

    Seperated pages

--- Cravings/Cravings/settings.py
+++ Cravings/Cravings/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'Cravings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
