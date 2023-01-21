f8611a91a41fc9a0b5950559081e13c4f87c2ee3
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-01-21 20:07:01 +0530

    Created Restaurant, MenuItem Model

--- Cravings/Shop/models.py
+++ Cravings/Shop/models.py
@@ -11,33 +11,36 @@ class User(models.Model):
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    address = models.TextField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email = models.EmailField()
-    cuisine = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='restaurant_images/', blank=True)
-    description = models.TextField()
+    image = models.ImageField(upload_to='Cravings/static/restaurantImages/', blank=True)
+    description = models.TextField(blank=True)
     is_open = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-# class MenuItem(models.Model):
-#     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=255)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-#     image = models.ImageField(upload_to='menu_item_images/', blank=True)
-#     is_vegetarian = models.BooleanField(default=False)
-#     is_vegan = models.BooleanField(default=False)
-#     is_gluten_free = models.BooleanField(default=False)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='Cravings/static/menu_item_images/', blank=True)
+    is_vegetarian = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 # class Order(models.Model):
 #     user = models.ForeignKey(User, on_delete=models.CASCADE)
 #     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
 #     items = models.ManyToManyField(MenuItem, through='OrderItem')
-#     total_cost = models.DecimalField(max_digits=6, decimal_places=2)
+#     total_cost = models.IntegerField()
 #     created_at = models.DateTimeField(auto_now_add=True)
 #     updated_at = models.DateTimeField(auto_now=True)
 
@@ -47,3 +50,15 @@ class Restaurant(models.Model):
 #     quantity = models.IntegerField()
 #     created_at = models.DateTimeField(auto_now_add=True)
 #     updated_at = models.DateTimeField(auto_now=True)
+
+# class Driver(models.Model):
+#     firstName = models.CharField(max_length=15)
+#     lastName = models.CharField(max_length=15)
+#     email = models.EmailField(null=True, blank=True)
+#     phone = models.CharField(max_length=255, blank=True)
+
+#     def __str__(self):
+#         return self.firstName+" "+self.lastName
+
+# class OrderStatus(models.Model):
+#     class Status(models.TextChoices):
